31539f0cc1e126fb4d70d5ff1a57cb924650eae6
toshiki-o23 <tsk.okmt.15@gmail.com>
2020-07-14 09:08:00 +0900

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'francophone-moose-82803.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
